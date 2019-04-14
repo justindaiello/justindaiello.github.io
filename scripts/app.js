6c69efdda3e1d35ed80ef908114c38fb94898839
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-04-14 13:49:10 -0400

    styled and positioned divs in the body. Added ajax calls to populate cleric information

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,14 @@ $(() => {
       (data)=>{
         $('.photo').attr('src', "css/images/cleric.png")
         $('.class-info h1').text(`${data.results[2].name}:`.toUpperCase());
-        $('.class-info p').text(data.results[2].desc)
+        $('.class-info p').text(data.results[2].desc);
+        $('.dice').text(' ' + data.results[2].hit_dice);
+        $('.spells').text(' ' + data.results[2].spellcasting_ability);
+        $('.prof').text(' ' + data.results[2].prof_saving_throws);
+        $('.armor').text(' ' + data.results[2].prof_armor);
+        $('.skills').text(' ' + data.results[2].prof_skills);
+        $('.box-2 span').text(' ' + data.results[2].archetypes[0].name);
+        $('.box-2 p').text(' ' + data.results[2].archetypes[0].desc);
       },
       ()=>{
           console.log('bad');
