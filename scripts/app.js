b0705be716de1c68a6d1261e140717bf051a0a67
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-04-14 16:50:34 -0400

    added more styling to body, made a footer. Created a function to call API data on all classes and append to the DOM

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,51 @@
 console.log('script is connected')
 
-$(() => {
 
+//==================================================
+//Function to grab API data and append it to the DOM
+//Arguments take the index number of the class and the location of the classes photo
+//API into is pulled and appened to various places in the DOM
+//===================================================
+const $getCharInfo = (index, pic) => {
   $.ajax({
       url: "https://api-beta.open5e.com/classes/"
   }).then(
       (data)=>{
-        $('.photo').attr('src', "css/images/cleric.png")
-        $('.class-info h1').text(`${data.results[2].name}:`.toUpperCase());
-        $('.class-info p').text(data.results[2].desc);
-        $('.dice').text(' ' + data.results[2].hit_dice);
-        $('.spells').text(' ' + data.results[2].spellcasting_ability);
-        $('.prof').text(' ' + data.results[2].prof_saving_throws);
-        $('.armor').text(' ' + data.results[2].prof_armor);
-        $('.skills').text(' ' + data.results[2].prof_skills);
-        $('.box-2 span').text(' ' + data.results[2].archetypes[0].name);
-        $('.box-2 p').text(' ' + data.results[2].archetypes[0].desc);
+        $('.photo').attr('src', pic).hide().fadeIn(1000)
+        $('.class-info h1').text(`${data.results[index].name}:`.toUpperCase()).hide().fadeIn(2000);
+        $('.class-info p').text(data.results[index].desc).hide().fadeIn(2000);
+        $('.dice').text(' ' + data.results[index].hit_dice).hide().fadeIn(2500);
+        $('.spells').text(' ' + data.results[index].spellcasting_ability).hide().fadeIn(2500);
+        $('.prof').text(' ' + data.results[index].prof_saving_throws).hide().fadeIn(2500);
+        $('.armor').text(' ' + data.results[index].prof_armor).hide().fadeIn(2500);
+        $('.skills').text(' ' + data.results[index].prof_skills).hide().fadeIn(2500);
+        $('.box-2 span').text(' ' + data.results[index].archetypes[0].name).hide().fadeIn(2500);
+        $('.box-2 p').text(' ' + data.results[index].archetypes[0].desc).hide().fadeIn(2500);
       },
       ()=>{
           console.log('bad');
       }
   );
+}
 
+$(() => {
+//Fades in the NavBar upon page load
 $('.navbar').hide().fadeIn(2000);
 
+//Listener to pull Barbarian API data and add it to the DOM
+$('#barbarian').on('click', () => { $getCharInfo(0, "css/images/barbarian.png") })
 
+//Listener to pull Cleric API data and add it to the DOM
+$('#cleric').on('click', () => { $getCharInfo(2, "css/images/cleric.png") })
+
+//Listener to pull Druid API data and add it to the DOM
+$('#druid').on('click', () => { $getCharInfo(3, "css/images/druid.png") })
+
+//Listener to pull Monk API data and add it to the DOM
+$('#monk').on('click', () => { $getCharInfo(5, "css/images/monk.png") })
+
+//Listener to pull Warlock API data and add it to the DOM
+$('#warlock').on('click', () => { $getCharInfo(10, "css/images/warlock.png") })
 
 
 });
